ec4c79b2e64f4241561ea763b66d9d27d773e5d1
Berk Ergün <hasan.berk.ergun@hotmail.com>
2019-03-28 19:02:16 +0300

    First Commit For Unit Tests

---
+++ TestNinja/Fundamentals/User.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestNinja.Fundamentals
+{
+    public class User
+    {
+        public bool IsAdmin { get; set; }
+    }
+}
