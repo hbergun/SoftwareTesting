a95f1e31dd3356f3d62397e9f174247ecfccfaf1
Berk Ergün <hasan.berk.ergun@hotmail.com>
2019-03-28 21:39:42 +0300

    Added New Unit Tests

---
+++ TestNinja.UnitTests/ReservationTests.cs
@@ -0,0 +1,37 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestClass] //MSTest FW
+    public class ReservationTests
+    {
+        [TestMethod] //MSTest FW
+        // FORMAT : public void CanBeCancelledBy_Scenario_ExpectedBehavior()
+        public void CanBeCancelledBy_UserIsAdmin_ReturnTrue()
+        { //3A
+            //Arrange
+            Reservation reservation = new Reservation();
+
+            //Act
+            var Result = reservation.CanBeCancelledBy(new User { IsAdmin = true });
+
+            //Assert
+            Assert.IsTrue(Result);
+        }
+
+        [TestMethod]
+        public void CanBeCancelledBy_MadeByUser_ReturnTrue()
+        { //3A
+            //Arrange
+            Reservation reservation = new Reservation();
+            reservation.MadeBy = new User { IsAdmin = false };
+            //Act
+            var Result = reservation.CanBeCancelledBy(reservation.MadeBy);
+            //Assert
+            Assert.IsTrue(Result);
+        }
+
+
+    }
+}
