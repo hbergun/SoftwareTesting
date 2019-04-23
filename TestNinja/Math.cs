fd50b79fa515e5919e43e2f8baf1e6d227e197d2
Berk Ergün <hasan.berk.ergun@hotmail.com>
2019-04-23 23:53:55 +0300

    Max Method Added To Math Class

--- TestNinja/Math.cs
+++ TestNinja/Math.cs
@@ -4,7 +4,16 @@ using System.Text;
 
 namespace TestNinja
 {
-    class Math
+    public class Math
     {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+        public int Max(int a, int b)
+        {
+            return (a > b) ? a : b;
+        }
+
     }
 }
