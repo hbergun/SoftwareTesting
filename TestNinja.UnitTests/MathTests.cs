6995228c1a74cbc57b419015858567dc0aa4fdad
Berk Ergün <hasan.berk.ergun@hotmail.com>
2019-04-24 19:38:00 +0300

    Dependency Inject

--- TestNinja.UnitTests/MathTests.cs
+++ TestNinja.UnitTests/MathTests.cs
@@ -21,31 +21,31 @@ namespace TestNinja.UnitTests
         [Test]
          public void Add_WhenCalled_ReturnTheSumOfArguments()
         {
-            var math = new Math();
-            var result = math.Add(1, 2);
+            //var math = new Math();
+            var result = _math.Add(1, 2);
             Assert.That(result, Is.EqualTo(3));
         }
         [Test]
         public void Max_FirstArgumentIsGreaterThen_ReturnTheFirstArgument()
         {
-            var math = new Math();
-            var result = math.Max(2, 1);
+            //var math = new Math();
+            var result = _math.Max(2, 1);
             Assert.That(result, Is.EqualTo(2));
         }
 
         [Test]
         public void Max_SecondArgumentIsGreaterThen_ReturnTheSecondArgument()
         {
-            var math = new Math();
-            var result = math.Max(1, 2);
+            //var math = new Math();
+            var result = _math.Max(1, 2);
             Assert.That(result, Is.EqualTo(2));
         }
 
         [Test]
         public void Max_ArgumentsAreEqual_ReturnTheSameArgument()
         {
-            var math = new Math();
-            var result = math.Max(1, 1);
+            //var math = new Math();
+            var result = _math.Max(1, 1);
             Assert.That(result, Is.EqualTo(1));
         }
     }
