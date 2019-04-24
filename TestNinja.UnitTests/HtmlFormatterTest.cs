19aba9e3526df8f27078d0c559de3c67193579c8
Berk Ergün <hasan.berk.ergun@hotmail.com>
2019-04-25 01:42:17 +0300

    Added HtmlFormatter - Test

---
+++ TestNinja.UnitTests/HtmlFormatterTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestNinja.UnitTests
+{
+    public class HtmlFormatterTest
+    {
+        public string FormatAsBold(string content)
+        {
+            return $"<strong>{content}</strong>";
+        }
+    }
+}
---
+++ TestNinja/Fundamentals/HtmlFormatter.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestNinja.Fundamentals
+{
+    class HtmlFormatter
+    {
+    }
+}
