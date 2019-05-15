9604910a8216e9454776605e9cfb8dfd772e230f
Berk Ergün <hasan.berk.ergun@hotmail.com>
2019-05-15 03:04:31 +0300

    ErrorLoggerTests Class Edit

--- TestNinja.UnitTests/ErrorLoggerTests.cs
+++ TestNinja.UnitTests/ErrorLoggerTests.cs
@@ -1,10 +1,20 @@
-﻿using System;
+﻿using NUnit.Framework;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using TestNinja.Fundamentals;
 
 namespace TestNinja.UnitTests
 {
-    class ErrorLoggerTests
+    [TestFixture]
+    public class ErrorLoggerTests
     {
+        [Test]
+        public void Log_WhenCalled_SetTheLastErrorProperty()
+        {
+            var logger = new ErrorLogger();
+            logger.Log("a");
+            Assert.That(logger.LastError, Is.EqualTo("a"));
+        }
     }
 }
