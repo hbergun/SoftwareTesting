37ccb35d246a2d162a873af98f3c00ae09500ab8
Berk Ergün <hasan.berk.ergun@hotmail.com>
2019-05-26 03:01:22 +0300

    Added UnitTestMethod For ErrorLogger

--- TestNinja.UnitTests/ErrorLoggerTests.cs
+++ TestNinja.UnitTests/ErrorLoggerTests.cs
@@ -16,5 +16,18 @@ namespace TestNinja.UnitTests
             logger.Log("a");
             Assert.That(logger.LastError, Is.EqualTo("a"));
         }
+
+        [Test]
+        [TestCase(null)]
+        [TestCase("")]
+        [TestCase(" ")]
+        public void Log_InvalidInput_ThrowArgumentNullException(string invalidinput)
+        {
+            var logger = new ErrorLogger();
+            
+            Assert.That(() => logger.Log(invalidinput),Throws.ArgumentNullException);
+            //Assert.That(() => logger.Log(invalidinput), Throws.Exception.TypeOf<DivideByZeroException>); //For Specify Exception
+
+        }
     }
 }
