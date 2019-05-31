9cf3ce1277d477d8e7e3d7f4b36059e9505e23ab
Berk Ergün <hasan.berk.ergun@hotmail.com>
2019-05-31 23:53:43 +0300

    FizzBuzz Class Added

---
+++ TestNinja/Fundamentals/FizzBuzz.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestNinja.Fundamentals
+{
+    public class FizzBuzz
+    {
+        public static string GetOutPut(int number)
+        {
+            if ((number % 3 == 0) && (number % 5 == 0))
+                return "FizzBuzz";
+            if (number % 3 == 0)
+                return "Fizz";
+            if (number % 5 == 0)
+                return "Buzz";
+
+            return number.ToString();
+        }
+    }
+}
