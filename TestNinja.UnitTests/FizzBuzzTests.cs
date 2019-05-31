8dafc253270d0235ed4eea1dac13e87703ba38e1
Berk Ergün <hasan.berk.ergun@hotmail.com>
2019-05-31 23:54:08 +0300

    FizBuzzTests Class Added

---
+++ TestNinja.UnitTests/FizzBuzzTests.cs
@@ -0,0 +1,40 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public class FizzBuzzTests
+    {
+        [Test]
+        public void GetOutPut_InputIsDivisibleBy3And5_ReturnFizzBuzz()
+        {
+            var result = FizzBuzz.GetOutPut(15);
+            Assert.That(result, Is.EqualTo("FizzBuzz"));
+        }
+
+        [Test]
+        public void GetOutPut_InputIsDivisibleBy3Only_ReturnFizz()
+        {
+            var result = FizzBuzz.GetOutPut(3);
+            Assert.That(result, Is.EqualTo("Fizz"));
+        }
+
+        [Test]
+        public void GetOutPut_InputIsDivisibleBy5Only_ReturnBuzz()
+        {
+            var result = FizzBuzz.GetOutPut(5);
+            Assert.That(result, Is.EqualTo("Buzz"));
+        }
+
+        [Test]
+        public void GetOutPut_InputIsNotDivisibleBy3And5_ReturnTheSameNumber()
+        {
+            var result = FizzBuzz.GetOutPut(1);
+            Assert.That(result, Is.EqualTo("1"));
+        }
+    }
+}
