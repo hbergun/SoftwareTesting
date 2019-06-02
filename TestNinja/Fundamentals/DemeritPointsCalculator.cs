fa258eacc73bd9575e4a219b45bc82ea147d2228
Berk Ergün <hasan.berk.ergun@hotmail.com>
2019-06-02 23:33:24 +0300

    Demerit Calc Added

---
+++ TestNinja/Fundamentals/DemeritPointsCalculator.cs
@@ -0,0 +1,23 @@
+﻿
+using System;
+
+namespace TestNinja.Fundamentals
+{
+    public class DemeritPointsCalculator
+    {
+        private const int SPEEDLIMIT = 65;
+        public int CalculateDemeritPoints(int speed)
+        {
+            if (speed < 0)
+                throw new ArgumentOutOfRangeException();
+
+            if (speed <= SPEEDLIMIT)
+                return 0;
+
+            const int kmPerDemeritPoint = 5;
+            var demeritPoints = (speed - SPEEDLIMIT) / kmPerDemeritPoint;
+
+            return demeritPoints;
+        }
+    }
+}
