7df84a2a09ad595b6d11c91a26d180f813cd2ec2
Berk Ergün <hasan.berk.ergun@hotmail.com>
2019-06-03 17:19:30 +0300

    Demerit Calc Test OK

--- TestNinja/Fundamentals/DemeritPointsCalculator.cs
+++ TestNinja/Fundamentals/DemeritPointsCalculator.cs
@@ -6,9 +6,11 @@ namespace TestNinja.Fundamentals
     public class DemeritPointsCalculator
     {
         private const int SPEEDLIMIT = 65;
+        private const int MAXSPEED = 300;
+
         public int CalculateDemeritPoints(int speed)
         {
-            if (speed < 0)
+            if (speed < 0 || speed > MAXSPEED)
                 throw new ArgumentOutOfRangeException();
 
             if (speed <= SPEEDLIMIT)
