0c028c37e19841b3498fef1cb528444f436a649b
Berk Ergün <hasan.berk.ergun@hotmail.com>
2019-06-04 23:20:16 +0300

    Stack Tests Comp

--- TestNinja.UnitTests/StackTests.cs
+++ TestNinja.UnitTests/StackTests.cs
@@ -24,6 +24,84 @@ namespace TestNinja.UnitTests
             Assert.That(stack.Count, Is.EqualTo(1)); //stack.Count ?? Is it working properly? How To Make Sure ? ---> Let's Create a New Test Method
         }
 
-        
+
+        [Test]
+        public void Pop_EmptyStack_ThrowInvalidOperationException()
+        {
+            var stack = new Stack<string>();
+            Assert.That(() => stack.Pop(), Throws.InvalidOperationException);
+        }
+
+
+        [Test]
+        public void Pop_StackWithAFewObjects_ReturnObjectOnTheTop()
+        {
+            //Arrange 
+            var stack = new Stack<string>();
+            stack.Push("A");
+            stack.Push("B");
+            stack.Push("C");
+            //Act
+            var result = stack.Pop();
+            //Assert
+            Assert.That(result, Is.EqualTo("C"));
+        }
+
+        [Test]
+        public void Pop_StackWithAFewObjects_RemoveObjectOnTheTop()
+        {
+            //Arrange 
+            var stack = new Stack<string>();
+            stack.Push("A");
+            stack.Push("B");
+            stack.Push("C");
+            //Act
+            stack.Pop();
+            //Assert
+            Assert.That(stack.Count, Is.EqualTo(2)); //Maybe Peek ?
+        }
+
+
+        [Test]
+        public void Peek_EmptyStack_ThrowInvalidOperationException()
+        {
+            //Arrange 
+            var stack = new Stack<string>();
+            Assert.That(() => stack.Peek(), Throws.InvalidOperationException);
+
+
+        }
+
+
+        [Test]
+        public void Peek_StackWithObject_ReturnObjectOnTopOfTheStack()
+        {
+            //Arrange 
+            var stack = new Stack<string>();
+            stack.Push("A");
+            stack.Push("B");
+            stack.Push("C");
+            //Act
+            var result = stack.Peek();
+            //Assert
+            Assert.That(result, Is.EqualTo("C"));
+
+            //Assert.That(result, Is.EqualTo(_));
+        }
+
+
+        [Test]
+        public void Peek_StackWithObject_DoesNotRemoveTheObjectOnTopOfTheStack()
+        {
+            //Arrange 
+            var stack = new Stack<string>();
+            stack.Push("A");
+            stack.Push("B");
+            stack.Push("C");
+            //Act
+            stack.Peek();
+            //Assert
+            Assert.That(stack.Count, Is.EqualTo(3));
+        }
     }
 }
