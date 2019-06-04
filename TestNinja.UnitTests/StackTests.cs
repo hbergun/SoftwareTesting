11922f26b5fc9d57d82665f2aa2b3ec076e55bd8
Berk Ergün <hasan.berk.ergun@hotmail.com>
2019-06-04 21:16:14 +0300

    Fix

--- TestNinja.UnitTests/StackTests.cs
+++ TestNinja.UnitTests/StackTests.cs
@@ -1,10 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using NUnit.Framework;
+using TestNinja.Fundamentals;
 
 namespace TestNinja.UnitTests
 {
-    class StackTests
+    [TestFixture]
+    public class StackTests
     {
+
+        [Test]
+        public void Push_ArgumentIsNull_ThrowArgNullException()
+        {
+            var stack = new Stack<string>();
+
+            Assert.That(() => stack.Push(null), Throws.ArgumentNullException);
+        }
+
+
+        [Test]
+        public void Push_ValidArg_AddTheObjectToTheStack()
+        {
+            var stack = new Stack<string>();
+            stack.Push("A");
+            Assert.That(stack.Count, Is.EqualTo(1)); //stack.Count ?? Is it working properly? How To Make Sure ? ---> Let's Create a New Test Method
+        }
+
+        
     }
 }
