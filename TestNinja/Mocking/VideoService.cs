497282599a2d692571c7ea5c8d36c640a6a6db23
Berk Ergün <hasan.berk.ergun@hotmail.com>
2019-06-07 23:09:03 +0300

    Video Service Class Filled

--- TestNinja/Mocking/VideoService.cs
+++ TestNinja/Mocking/VideoService.cs
@@ -1,10 +1,31 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 
 namespace TestNinja.Mocking
 {
-    class VideoService
+    public class VideoService
     {
+        public string ReadVideoTitle()
+        {
+            var str = File.ReadAllText("video.txt");
+            var video = JsonConvert.DeserializeObject<Video>(str);
+            if (video == null)
+                return "Error Parsing The Video";
+            return video.Title;
+        }
+        public string GetUnprocessedVideoAsCsv()
+        {
+            var videoIds = new List<int>();
+            return "";
+        }
+    }
+    public class Video
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public bool Processed { get; set; }
     }
 }
